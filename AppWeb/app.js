d2ed5dc06d959b4e3593862297d50abe6f6d8f8e
MANRED12 <roral@hotmail.com>
2022-11-24 20:13:51 -0500

    instalacion de nodemon

--- AppWeb/app.js
+++ AppWeb/app.js
@@ -5,8 +5,21 @@ import express from "express";
 const app = express();
 const puerto = 3000;
 
-function callback(){
-    console.log("El servidor se esta ejecutando")
-}
+app.listen(puerto, () => {
+    console.log("El servidor se esta ejecutando.")
+})
 
-app.listen(puerto, callback)
+app.get("/home", (req, res) => {
+    //Implementamos código
+    res.send("Estas en Home!!")
+})
+
+app.get("/", (req, res) => {
+    //Implementamos código
+    res.send("Bienvenido Ing. Rodrigo!!")
+})
+
+app.get("/home/grupo", (req, res) => {
+    //Implementamos código
+    res.send("Bienvenidos a mi grupo")
+})
