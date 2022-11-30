6e0db7a935f4f3b96ef0ddfb536083fc6468c549
MANRED12 <roral@hotmail.com>
2022-11-29 23:48:37 -0500

    Creacion plantilla a la fecha

---
+++ AppWeb/Bolsillo Virtual/controllers/userController.js
@@ -0,0 +1,22 @@
+//CRUD
+export function createUser(res){
+    //Implementacion
+    res.status(200)
+    res.json({mensaje:"Usuario Creado"})
+    //res.end()
+}
+
+export function readUser(res){
+    //Implementacion
+    res.sendStatus(200)
+}
+
+export function updateUser(res){
+    //Implementacion
+    res.sendStatus(200)
+}
+
+export function deleteUser(res){
+    //Implementacion
+    res.sendStatus(200)
+}
