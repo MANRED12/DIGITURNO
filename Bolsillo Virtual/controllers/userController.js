169779c0495fef9bf92707576728c21bd515f3a4
MANRED12 <roral@hotmail.com>
2022-11-30 00:12:00 -0500

    Se agregan modificaciones a Bolsillo Virtual

---
+++ Bolsillo Virtual/controllers/userController.js
@@ -0,0 +1,17 @@
+export function createUSer(res){
+    //Implementacion
+    res.sendStatus(200)
+
+}
+export function readUSer(){
+    //Implementacion
+    res.sendStatus(200)
+}
+export function updateUSer(){
+    //Implementacion
+    res.sendStatus(200)
+}
+export function deleteUSer(){
+    //Implementacion
+    res.sendStatus(200)
+}
