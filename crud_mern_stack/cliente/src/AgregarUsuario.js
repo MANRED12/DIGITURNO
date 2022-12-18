23bcf3b669e01ec79e7ad28e4c8229906592cf1d
MANRED12 <roral@hotmail.com>
2022-12-18 07:51:27 -0500

    new commits

--- crud_mern_stack/cliente/src/AgregarUsuario.js
+++ crud_mern_stack/cliente/src/AgregarUsuario.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function AgregarUsuario(){
-    return(
-        <div>
-            <h2 style={{color: 'green'}}>CREAR UN NUEVO USUARIO</h2>
+function AgregarUsuario() {
+
+    function agregarusuario(){
+
+    }
+    
+    return (
+        <div className="container">
+            <div className="row">
+                <h2 className="mt-4" style={{ color: "green" }}>CREAR UN NUEVO USUARIO</h2>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-6" offset-3>
+                    <div className="mb-3">
+                        <label htmlFor="nombre" className="form-label">Nombre</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control"></input>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="telefono" className="form-label">Teléfono</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+
+                    <button onClick={AgregarUsuario} className="btn btn-success">Guardar Usuario</button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default AgregarUsuario
+export default AgregarUsuario;
---
+++ noteApp/src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+//Initializations
+const app = express()
+
+//Settings
+app.set('port', process.env.PORT || 3000)
+//Midslewres
+//Global variables
+//Routes
+//Static files
+//Server is listening
+app.listen(app.get('port'),()=>{
+    
+})
