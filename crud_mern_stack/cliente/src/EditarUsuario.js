99720de89cf6be1ac53972730e4bdcd15422fd32
MANRED12 <roral@hotmail.com>
2022-12-20 13:38:04 -0500

    Cambio EditarUsuario

---
+++ crud_mern_stack/cliente/src/EditarUsuario.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EditarUsuario(){
+    return(
+        <div>
+            <h2 style={{color:'pink'}}>Editar un usuario existente</h2>
+        </div>
+
+    )
+}
+
+export default EditarUsuario
